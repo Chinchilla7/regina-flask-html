d7e9cb029f2d7dfa6b5e5986f23257104261407f
Chinchilla7 <regina.chen@stonybrook.edu>
2022-09-14 21:27:28 -0400

    Update app.py

--- Assignment 3a/app.py
+++ Assignment 3a/app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello world, WWW!'
-
 @app.route('/')
 def home_page():
     return render_template('homepage.html')
