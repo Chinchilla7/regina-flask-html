ede124270c3f10feeefd81faf02098bc2e0df84a
Chinchilla7 <regina.chen@stonybrook.edu>
2022-09-16 14:24:32 -0400

    Update app.py

--- Assignment 3a/app.py
+++ Assignment 3a/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#creating and styling homepage using flask render_template homepage and styles2
 @app.route('/')
 def home_page():
     return render_template('homepage.html')
+
+#creating and styling contact page using flask render_template contact and styles3
 
 @app.route('/contactus')
 def contact_page():
